5ec44fb9aa3d8f3ec173436c2c0c521d76258645
mohamed <mmm.medox@gmail.com>
2025-07-30 20:35:51 +0300

    add splash screen UI

---
+++ lib/core/theming/my_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class MyColors {
+  static const Color primaryColor = Color(0xff003500);
+}
---
+++ lib/features/splash/ui/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_market/assets/assets.dart';
+import 'package:fruits_market/core/theming/my_colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(milliseconds: 1)).then((value) {
+      // Navigator.pushReplacementNamed(context, );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyColors.primaryColor,
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Image.asset(Assets.imagesPngSplash, fit: BoxFit.fill),
+      ),
+    );
+  }
+}
