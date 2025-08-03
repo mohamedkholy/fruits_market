4e21b93949dc02921af1c7a142bf25229612507f
mohamed <mmm.medox@gmail.com>
2025-08-03 22:09:01 +0300

    change the bottom navigation bar

--- lib/features/navigation/ui/navigation_screen.dart
+++ lib/features/navigation/ui/navigation_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_market/core/theming/my_colors.dart';
+import 'package:fruits_market/core/theming/my_text_styles.dart';
 import 'package:fruits_market/features/home/ui/home_screen.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class NavigationScreen extends StatefulWidget {
   const NavigationScreen({super.key});
@@ -27,44 +29,56 @@ class _NavigationScreenState extends State<NavigationScreen> {
           topLeft: Radius.circular(25),
           topRight: Radius.circular(25),
         ),
-        child: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: MyColors.primaryColor,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: MyColors.grey,
-          selectedFontSize: 1,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          selectedIconTheme: const IconThemeData(size: 27),
-          unselectedIconTheme: const IconThemeData(size: 25),
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(IconData(0xe817, fontFamily: "navigation")),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(IconData(0xe810, fontFamily: "navigation")),
-              label: 'Ranks',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(IconData(0xe814, fontFamily: "navigation"), size: 32),
-              label: 'Cart',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(IconData(0xe812, fontFamily: "navigation")),
-              label: 'Favorites',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(IconData(0xe811, fontFamily: "navigation")),
-              label: 'Profile',
-            ),
-          ],
+        child: Container(
+          height: 80,
+          color: MyColors.primaryColor,
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+          child: GNav(
+            mainAxisAlignment: MediaQuery.of(context).size.width < 600
+                ? MainAxisAlignment.spaceBetween
+                : MainAxisAlignment.spaceEvenly,
+            tabBorderRadius: 35,
+            duration: const Duration(milliseconds: 100),
+            gap: 8,
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+            textStyle: MyTextStyles.font24BoldPrimary.copyWith(fontSize: 15),
+            color: MyColors.grey,
+            activeColor: MyColors.primaryColor,
+            tabBackgroundColor: Colors.white,
+            selectedIndex: _currentIndex,
+            onTabChange: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            tabs: [
+              GButton(
+                iconSize: _currentIndex == 0 ? 18 : 27,
+                icon: const IconData(0xe817, fontFamily: "navigation"),
+                text: 'Home',
+              ),
+              GButton(
+                iconSize: _currentIndex == 1 ? 18 : 27,
+                icon: const IconData(0xe810, fontFamily: "navigation"),
+                text: 'Orders',
+              ),
+              GButton(
+                iconSize: _currentIndex == 2 ? 22 : 32,
+                icon: const IconData(0xe814, fontFamily: "navigation"),
+                text: 'Cart',
+              ),
+              GButton(
+                iconSize: _currentIndex == 3 ? 18 : 27,
+                icon: const IconData(0xe812, fontFamily: "navigation"),
+                text: 'Favorites',
+              ),
+              GButton(
+                iconSize: _currentIndex == 4 ? 18 : 27,
+                icon: const IconData(0xe811, fontFamily: "navigation"),
+                text: 'Profile',
+              ),
+            ],
+          ),
         ),
       ),
     );
